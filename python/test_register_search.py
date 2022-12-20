fd9bbd335d8a5534395354b3f8e4fa7e78bc40cd
ming@zmeet <jiaozhu@abcpen.com>
2022-12-20 23:37:00 +0800

    update doc

--- python/test_register_search.py
+++ python/test_register_search.py
@@ -1,16 +1,15 @@
-
 import requests
 
 def audio_register():
     files = {'audio': open('./777-126732-0003.flac', 'rb')}
-    values = {'spk_name': 'jiaozhu', "org_id": "abcpen", "tag_id": "tech"}
+    values = {'spk_name': 'jiaozhu', "app_id": "abcpen", "tag_id": "tech"}
     url = "https://translate.abcpen.com/audio/register"
     r = requests.post(url, files=files, data=values)
     print(r.text)
 
 def audio_search():
     files = {'audio': open('./777-126732-0003.flac', 'rb')}
-    values = {"org_id": "abcpen"}
+    values = {"app_id": "abcpen"}
     url = "https://translate.abcpen.com/audio/search"
     r = requests.post(url, files=files, data=values)
     print(r.text)
