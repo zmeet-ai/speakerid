3086312597b35150a4a1d88818c58931f4c4972b
ming@zmeet <jiaozhu@abcpen.com>
2022-12-21 08:50:56 +0800

    update doc

--- python/test_register_search.py
+++ python/test_register_search.py
@@ -1,19 +1,19 @@
 import requests
 
 def audio_register():
-    files = {'audio': open('./777-126732-0003.flac', 'rb')}
+    files = {'audio': open('./777-126732-0004.flac', 'rb')}
     values = {'spk_name': 'jiaozhu', "app_id": "abcpen", "tag_id": "tech"}
-    url = "https://translate.abcpen.com/audio/register"
+    url = "https://voiceid.abcpen.com/voiceid/register"
     r = requests.post(url, files=files, data=values)
     print(r.text)
 
 def audio_search():
-    files = {'audio': open('./777-126732-0003.flac', 'rb')}
+    files = {'audio': open('./777-126732-0004.flac', 'rb')}
     values = {"app_id": "abcpen"}
-    url = "https://translate.abcpen.com/audio/search"
+    url = "https://voiceid.abcpen.com/voiceid/recognize"
     r = requests.post(url, files=files, data=values)
     print(r.text)
 
 if __name__ == "__main__":
-    audio_register()
+    #audio_register()
     audio_search()
